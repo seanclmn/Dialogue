eb61966fb3a560134b461b7f72545e7c212bcf20
Sean Coleman <seanclmn1@gmail.com>
2024-08-01 23:56:23 +0900

    word wrap

--- client/src/components/shared/Messages/Message.tsx
+++ client/src/components/shared/Messages/Message.tsx
@@ -8,13 +8,13 @@ export const Message = ({ text, senderIsMe }: MessageProps) => {
   return (
     <div
       className={`${
-        senderIsMe ? "bg-purple-700 ml-auto max-w-40" : "bg-gray-200"
+        senderIsMe ? "bg-purple-700 ml-auto max-w-[100%]" : "bg-gray-200"
       } my-[1px] p-1 rounded-[10px] inline-flex`}
     >
       <p
         lang="en"
-        className={`px-2 whitespace-break-spaces break-words	
-          text-[15px] hyphens-auto	 ${senderIsMe ? "text-white" : ""}`}
+        className={`px-2 whitespace-break-spaces text-wrap
+          text-[15px] 	 ${senderIsMe ? "text-white" : ""}`}
       >
         {text}
       </p>
