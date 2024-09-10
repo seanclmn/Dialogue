d0c5ced918cbc19aa4a6f299fcde11eaf1deed52
Sean Coleman <seanclmn1@gmail.com>
2024-09-10 23:55:22 +0900

    ts server

---
+++ ts-server/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      sortSchema: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
