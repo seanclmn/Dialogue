a65302876c6fbfa401abbbf6e29840aea11672f4
seanclmn <seanclmn1@gmail.com>
2026-05-14 23:30:53 -0400

    change base route in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [relay, react()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       //@ts-ignore
