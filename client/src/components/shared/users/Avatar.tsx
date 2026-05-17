d6bbfa96c36b05bca31f97edd61102023de70fc8
seanclmn <seanclmn1@gmail.com>
2026-05-17 13:23:32 -0400

    Update style system

--- client/src/components/shared/users/Avatar.tsx
+++ client/src/components/shared/users/Avatar.tsx
@@ -41,7 +41,7 @@ export const Avatar = ({
       <div className="relative inline-block">
         {body}
         <Link to={link ?? ""}>
-          <div className="absolute inset-0 flex cursor-pointer items-center justify-center rounded-full bg-black/50">
+          <div className="absolute inset-0 flex cursor-pointer items-center justify-center rounded-full bg-black/50 opacity-0 hover:opacity-100 transition-opacity duration-100">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6" fill="white">
               <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
             </svg>
